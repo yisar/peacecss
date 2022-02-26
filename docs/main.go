9fe5cce830567529fc42e67d50efda5045903a67
yisar <1533540012@qq.com>
2022-02-26 16:07:31 +0800

    wasm

--- docs/main.go
+++ docs/main.go
@@ -1,40 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	// "syscall/js"
+	"syscall/js"
+
 	nextcss "github.com/yisar/nextcss"
 )
 
+func JSONStringfy(data []*nextcss.CSSDefinition) string {
+	ret, _ := json.MarshalIndent(data, "", "  ")
+	return string(ret)
+}
+
 func main() {
-	parser := nextcss.NewParser()
+	// parser := nextcss.NewParser()
 
-	s := []byte(".a{color:#fff;}")
+	// s := []byte(".a{color:#fff;}")
 
-	ast := parser.Parse(s)
+	// ast := parser.Parse(s)
 	
-	ast.Walk(func (node *nextcss.CSSDefinition){
-		fmt.Printf("before: %v\n", node)
+	// ast.Walk(func (node *nextcss.CSSDefinition){
+	// 	fmt.Printf("before: %v\n", node)
 		
-		node.Selector.Selector = ".b"
+	// 	node.Selector.Selector = ".b"
 	
-		fmt.Printf("after: %v\n", node)
-	})
+	// 	fmt.Printf("after: %v\n", node)
+	// })
 
-	ast.Minisize()
+	// ast.Minisize()
 
 	// json := ast.ToPrettyJSON()
 
 	// fmt.Printf("ast: %s\n", json)
 
-	// js.Global().Set("cssnextParse", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-	// 	parser := cssnext.NewParser()
-	// 	s := []byte(args[0].String())
-	// 	ast := parser.Parse(s)
+	js.Global().Set("nextcssParse", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		parser := nextcss.NewParser()
+		s := []byte(args[0].String())
+		ast := parser.Parse(s)
+		out := JSONStringfy(ast.GetData())
+		return out
+	}))
 
-	// 	out := ast.ToPrettyJSONString()
-	// 	return out
-	// }))
+	js.Global().Set("nextcssMinisize", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		parser := nextcss.NewParser()
+		s := []byte(args[0].String())
+		ast := parser.Parse(s)
+		out := ast.Minisize()
+		return out.String()
+	}))
 
-	// select {}
+	select {}
 }
